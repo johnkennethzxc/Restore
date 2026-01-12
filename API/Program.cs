b3305eafd2e16153605da7174eab54af4a2c1fe3
Ken <bombanejohnkenneth01@gmail.com>
2026-01-12 08:38:48 -0500

    End of section 7

--- API/Program.cs
+++ API/Program.cs
@@ -20,7 +20,7 @@ app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseCors(opt =>
 {
-    opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000");
+    opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("https://localhost:3000");
 });
 
 app.MapControllers();
